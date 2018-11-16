3e27c07b8c6a1890032db8428c298b59ff4742d0
sansanda <dsanchezsanc@uoc.edu>
2018-11-16 13:24:40 +0100

    Creating live filtering for entities

--- myapplication/src/main/java/com/example/myapplication/MyUI.java
+++ myapplication/src/main/java/com/example/myapplication/MyUI.java
@@ -6,14 +6,18 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -27,22 +31,38 @@ public class MyUI extends UI {
 
 	private CustomerService customerService = CustomerService.getInstance();
 	private Grid<Customer> grid = new Grid<Customer>(Customer.class);
+	private TextField filterCustomerListTextField = new TextField();
+	
 	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         
-    	final VerticalLayout layout = new VerticalLayout();
-        layout.addComponent(grid);
+    	updateCustomers();
+    	
+    	final VerticalLayout verticalLayout = new VerticalLayout();
         grid.setColumns("firstName","lastName","email","birthDate");
         
-        updateCustomers();
+        filterCustomerListTextField.setPlaceholder("filter by name...");
+        filterCustomerListTextField.addValueChangeListener(e->updateCustomers());
+        filterCustomerListTextField.setValueChangeMode(ValueChangeMode.LAZY);
+        
+        Button clearFilterCustomerListTextField = new Button(VaadinIcons.CLOSE);
+        clearFilterCustomerListTextField.setDescription("Clear the filter text");
+        clearFilterCustomerListTextField.addClickListener(e->filterCustomerListTextField.clear());
+        
+        CssLayout filteringLayout = new CssLayout();
+        filteringLayout.addComponents(filterCustomerListTextField,clearFilterCustomerListTextField);
+        filteringLayout.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
         
         
-        setContent(layout);
+        
+        verticalLayout.addComponents(filteringLayout,grid);
+        
+        setContent(verticalLayout);
     }
 
 	private void updateCustomers() {
-		List<Customer> customers = customerService.findAll();	
+		List<Customer> customers = customerService.findAll(filterCustomerListTextField.getValue());	
         grid.setItems(customers);
 	}
 
